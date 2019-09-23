64ae5d7f179aa6b214b276b8c44dde070fe257f3
mwolicki <mikolajw1997@gmail.com>
2019-09-23 20:26:48 +0200

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
 const router = require('./src/router/router');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
 app.use('/', router);
